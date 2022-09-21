31a6514f9fa8b728ebbb7fc0da1bbf2ed876c733
Nomesh-Gaur <113971266+Nomesh-Gaur@users.noreply.github.com>
2022-09-21 19:30:36 +0530

    Create ARRAY.cpp

---
+++ ASSIGNMENT 1/C++/ARRAY.cpp
@@ -0,0 +1,25 @@
+https://www.hackerrank.com/challenges/arrays-introduction/problem?isFullScreen=true
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int n;
+    cin>>n;
+    int a[n];
+    int c=a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int j =n-1;j>=0;j--){
+        cout<<a[j]<<" ";
+    }
+    return 0;
+}
+
