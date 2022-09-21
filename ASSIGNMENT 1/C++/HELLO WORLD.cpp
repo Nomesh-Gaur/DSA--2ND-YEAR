89c07cfa7807aa7a5d69d31bc8d106d7e9d3359d
Nomesh-Gaur <113971266+Nomesh-Gaur@users.noreply.github.com>
2022-09-21 19:21:34 +0530

    Create HELLO WORLD.cpp

---
+++ ASSIGNMENT 1/C++/HELLO WORLD.cpp
@@ -0,0 +1,11 @@
+https://www.hackerrank.com/challenges/cpp-hello-world/submissions/code/289355554
+
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
